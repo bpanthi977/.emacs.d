1982670e151f57bb8127c64c7f4e504802e911b0
Bibek Panthi <bpanthi977@gmail.com>
2020-08-24 13:03:53 +0545

    back to slime.

--- modules/lisp-config.el
+++ modules/lisp-config.el
@@ -1,67 +1,74 @@
 ;;Common Lisp
-;; (use-package slime
-;;   :ensure t
-;;   :commands (slime)
-;;   :hook (( slime-mode . (lambda ()
-;; 						  (setf fill-column 100)
-;; 						  (add-to-list 'company-backends 'company-slime))))
-;;   :config 
-;;   (setq inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core" ;; --dynamic-space-size 2560"
-;; 	slime-contribs '(slime-asdf slime-banner slime-fancy slime-hyperdoc slime-macrostep slime-mrepl slime-quicklisp slime-sbcl-exts slime-scratch slime-sprof slime-xref-browser slime-company)
-;; 	common-lisp-hyperspec-root "~/Dev/lisp/HyperSpec-7-0/HyperSpec/")
 
-;;   ;; (defun slime-ecl ()
-;;   ;;   (interactive)
-;;   ;;   (slime "/usr/bin/ecl"))
 
-;;   (defun view-sdl-doc ()
-;;     (interactive)
-;;     (eww-open-file "~/Dev/lisp/quicklisp/dists/quicklisp/software/lispbuilder-20180831-git/lispbuilder-sdl/documentation/lispbuilder-sdl.html"))
-;;   )
+(use-package slime
+  :ensure t
+  :commands (slime)
+  :hook (( slime-mode . (lambda ()
+			  (wolfe/pretty-symbol-push-default)
+			  (prettify-symbols-mode)
+			  (setf fill-column 100)
+			  (add-to-list 'company-backends 'company-slime))))
+:config
+    ;;; To save new core run the script at
+  ;;; ~/Development/lisp/save-new-core.lisp
+(cond ((string-equal system-type "windows-nt")
+       (setf inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core" ;; --dynamic-space-size 2560
+	     ))
+      (t
+       (setf inferior-lisp-program "sbcl --core /home/bpanthi/.cache/common-lisp/core"
+	     common-lisp-hyperspec-root "file:///home/bpanthi/Dev/lisp/HyperSpec-7-0/HyperSpec/")))
+
+(setq	slime-contribs '(slime-asdf slime-banner slime-fancy slime-hyperdoc slime-macrostep slime-mrepl slime-quicklisp slime-sbcl-exts slime-scratch slime-sprof slime-xref-browser slime-company))
+
+;; (defun view-sdl-doc ()
+;;   (interactive)
+;;   (eww-open-file "~/Dev/lisp/quicklisp/dists/quicklisp/software/lispbuilder-20180831-git/lispbuilder-sdl/documentation/lispbuilder-sdl.html"))
+;; )
 
 ;; (when  (string-equal system-type "windows-nt")
 ;;   (setq inferior-lisp-program "sbcl")))
-
+)
 
 
 ;; (use-package slime-company
 ;;   :ensure t)
 
 
-(use-package sly
-  :defer t 
-  :ensure t
-  ;; :requires (smartparens)
-  :commands (sly)
-  :bind (:map sly-prefix-map
-	      ("M-p" . sly-mrepl-set-package)
-	      :map lisp-mode-map
-	      ("M-m d s" . sly-documentation))
-  :hook (lisp-mode . (lambda ()
-		       (wolfe/pretty-symbol-push-default)
-		       (prettify-symbols-mode)))
-  :config
+;; (use-package sly
+;;   :defer t 
+;;   :ensure t
+;;   ;; :requires (smartparens)
+;;   :commands (sly)
+;;   :bind (:map sly-prefix-map
+;; 	      ("M-p" . sly-mrepl-set-package)
+;; 	      :map lisp-mode-map
+;; 	      ("M-m d s" . sly-documentation))
+;;   :hook (lisp-mode . (lambda ()
+;; 		       (wolfe/pretty-symbol-push-default)
+;; 		       (prettify-symbols-mode)))
+;;   :config
 
-  ;;; To save new core run the script at
-  ;;; ~/Development/lisp/save-new-core.lisp
-  (cond ((string-equal system-type "windows-nt")
-	 (setf inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core" ;; --dynamic-space-size 2560
-	       ))
-	(t
-	 (setf inferior-lisp-program "sbcl --core /home/bpanthi/.cache/common-lisp/core"
-	       common-lisp-hyperspec-root "file:///home/bpanthi/Dev/lisp/HyperSpec-7-0/HyperSpec/")))
-  :init 
-  (defun sly-select-core ()
-    (interactive)
-    (let* ((dir (if windows-system?
-		    "C:/Users/hp/.cache/common-lisp/"
-		  "/home/bpanthi/.cache/common-lisp/"))
-	   (files (directory-files dir nil "core"))
-	   (core (completing-read "Core: " files)))
-      (when core
-	(setf inferior-lisp-program (format "sbcl --core %s%s" dir core))
-	(sly))))
-  )
+;;   ;;; To save new core run the script at
+;;   ;;; ~/Development/lisp/save-new-core.lisp
+;;   (cond ((string-equal system-type "windows-nt")
+;; 	 (setf inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core" ;; --dynamic-space-size 2560
+;; 	       ))
+;; 	(t
+;; 	 (setf inferior-lisp-program "sbcl --core /home/bpanthi/.cache/common-lisp/core"
+;; 	       common-lisp-hyperspec-root "file:///home/bpanthi/Dev/lisp/HyperSpec-7-0/HyperSpec/")))
+;;   :init 
+;;   (defun sly-select-core ()
+;;     (interactive)
+;;     (let* ((dir (if windows-system?
+;; 		    "C:/Users/hp/.cache/common-lisp/"
+;; 		  "/home/bpanthi/.cache/common-lisp/"))
+;; 	   (files (directory-files dir nil "core"))
+;; 	   (core (completing-read "Core: " files)))
+;;       (when core
+;; 	(setf inferior-lisp-program (format "sbcl --core %s%s" dir core))
+;; 	(sly))))
+;;   )
 
 ;;  (setf inferior-lisp-program "clisp"))
 ;; (setf inferior-lisp-program "sbcl --core c:/Users/hp/lack-core"))
