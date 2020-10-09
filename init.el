55a31b4aad70d00756823b43aaab6ad7ce9e81e5
Bibek Panthi <bpanthi977@gmail.com>
2020-10-09 20:26:35 +0545

    other small modes

--- init.el
+++ init.el
@@ -58,6 +58,7 @@
    "web-config"
    "latex-config"
    "js-config"
+   "other-modes"
    "tools"
    ))
 
---
+++ modules/other-modes.el
@@ -0,0 +1,5 @@
+(use-package pdf-tools 
+  :ensure t 
+  :defer t 
+  :init 
+  (pdf-loader-install))
