d5e4b334f165dfb90b445482b958f57190ac56d7
Bibek Panthi <bpanthi977@gmail.com>
2021-03-26 19:53:54 +0545

    cleaned lisp and python config

--- modules/lisp-config.el
+++ modules/lisp-config.el
@@ -1,9 +1,7 @@
 ;;Common Lisp
-
-
 (use-package slime
   :ensure t
-  :commands (slime)
+  :commands (slime cl-services)
   :hook (( slime-mode . (lambda ()
 			  (wolfe/pretty-symbol-push-default)
 			  (push '("defun"    . ?ƒ) prettify-symbols-alist)
@@ -13,82 +11,36 @@
 			  (setf fill-column 100)
 			  (add-to-list 'company-backends 'company-slime))))
   :config
+  (bind-keys :map lisp-mode-map
+	     ("M-m d" . slime-documentation))
+  
   (defun bp/log4cl () 
     (interactive)
     (load "/mnt/Data/Dev/lisp/quicklisp/log4slime-setup.el")
     (global-log4slime-mode 1))
-    ;;; To save new core run the script at
-  ;;; ~/Development/lisp/save-new-core.lisp
+  
   (cond ((string-equal system-type "windows-nt")
-	 (setf inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core" ;; --dynamic-space-size 2560
-	       ))
+	 ;; --dynamic-space-size 2560
+	 (setf inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core"))
 	(t
 	 (setf inferior-lisp-program "sbcl --core /home/bpanthi/.cache/common-lisp/core"
 	       common-lisp-hyperspec-root "file:///home/bpanthi/Dev/lisp/HyperSpec-7-0/HyperSpec/")))
 
-  (setq	slime-contribs '(slime-asdf slime-banner slime-fancy slime-hyperdoc slime-macrostep slime-mrepl slime-quicklisp slime-sbcl-exts slime-scratch slime-sprof slime-xref-browser slime-company))
-
-  ;; (defun view-sdl-doc ()
-  ;;   (interactive)
-  ;;   (eww-open-file "~/Dev/lisp/quicklisp/dists/quicklisp/software/lispbuilder-20180831-git/lispbuilder-sdl/documentation/lispbuilder-sdl.html"))
-  ;; )
-
-  ;; (when  (string-equal system-type "windows-nt")
-  ;;   (setq inferior-lisp-program "sbcl")))
-
+  (setq	slime-contribs '(slime-asdf slime-banner slime-fancy slime-hyperdoc
+				    slime-macrostep slime-mrepl slime-quicklisp
+				    slime-sbcl-exts slime-scratch slime-sprof
+				    slime-xref-browser slime-company))
   (defun cl-services () 
     (interactive)
     (let ((port (string-to-number (with-temp-buffer 
 				    (insert-file-contents "/tmp/cl-services-swank")
 				    (buffer-string)))))
       (slime-connect "localhost" port)))
-  )
 
-
-;; (use-package slime-company
-;;   :ensure t)
-
-
-;; (use-package sly
-;;   :defer t 
-;;   :ensure t
-;;   ;; :requires (smartparens)
-;;   :commands (sly)
-;;   :bind (:map sly-prefix-map
-;; 	      ("M-p" . sly-mrepl-set-package)
-;; 	      :map lisp-mode-map
-;; 	      ("M-m d s" . sly-documentation))
-;;   :hook (lisp-mode . (lambda ()
-;; 		       (wolfe/pretty-symbol-push-default)
-;; 		       (prettify-symbols-mode)))
-;;   :config
-
-;;   ;;; To save new core run the script at
-;;   ;;; ~/Development/lisp/save-new-core.lisp
-;;   (cond ((string-equal system-type "windows-nt")
-;; 	 (setf inferior-lisp-program "sbcl --core C:/Users/hp/.cache/common-lisp/core" ;; --dynamic-space-size 2560
-;; 	       ))
-;; 	(t
-;; 	 (setf inferior-lisp-program "sbcl --core /home/bpanthi/.cache/common-lisp/core"
-;; 	       common-lisp-hyperspec-root "file:///home/bpanthi/Dev/lisp/HyperSpec-7-0/HyperSpec/")))
-;;   :init 
-;;   (defun sly-select-core ()
-;;     (interactive)
-;;     (let* ((dir (if windows-system?
-;; 		    "C:/Users/hp/.cache/common-lisp/"
-;; 		  "/home/bpanthi/.cache/common-lisp/"))
-;; 	   (files (directory-files dir nil "core"))
-;; 	   (core (completing-read "Core: " files)))
-;;       (when core
-;; 	(setf inferior-lisp-program (format "sbcl --core %s%s" dir core))
-;; 	(sly))))
-;;   )
-
-;;  (setf inferior-lisp-program "clisp"))
-;; (setf inferior-lisp-program "sbcl --core c:/Users/hp/lack-core"))
-;; :hook ((mrepl-mode . smartparens-mode)))
-
-
+  (defun slime-ccl ()
+    (interactive)
+    (let ((inferior-lisp-program "ccl"))
+      (slime))))
 
 ;; Emacs Lisp
 (use-package elisp-slime-nav
@@ -105,6 +57,10 @@
   :bind (:map emacs-lisp-mode-map 
 	      ("C-c C-c" . eval-defun)
 	      ("C-c C-l" . eval-buffer)))
+
+(use-package elisp-mode
+  :config 
+  (define-key lisp-interaction-mode-map (kbd "C-j") #'electric-newline-and-maybe-indent))
 
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/maxima/")
 (autoload 'maxima-mode "maxima" "Maxima mode" t)
--- modules/python-config.el
+++ modules/python-config.el
@@ -1,73 +1,36 @@
-;;(install-packages (list ;; 'flycheck
-
-
-;; 'anaconda-mode
-;; 'company-anaconda
-;; 'elpy
-;; 'company-jedi
-;;		   'jedi-core
-;;		   'helm-dash))
-
-;; (require 'elpy)
-;; (elpy-enable)
-
-;; (when (require 'flycheck nil t)
-;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-;; (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-(defun my-python-mode-hook ()
-  ;; (require 'elpy)
-  ;; (require 'flycheck)
-  ;; (flycheck-mode)
-  ;; (require 'anaconda-mode)
-  ;; (require 'company-anaconda)
-  ;; (elpy-modules-global-init)
-  ;; (elpy-mode t)
-  ;; (add-to-list 'company-backends 'company-elpy)
-  (setf python-environment-directory (concat init-dir "extra/python-environment"))
-  (require 'helm-dash)
-  (require 'company-jedi)
-  ;; (require 'company-jedi)
-  ;; (require 'jedi-core)
-  ;; (setq jedi:complete-on-dot t)
-  (add-to-list 'company-backends 'company-jedi)
-  ;; (add-to-list 'company-backends 'company-anaconda)
-  ;; (anaconda-eldoc-mode t)
-  ;; (anaconda-mode t)
-  (helm-dash-activate-docset "Python_3")
-  (helm-dash-activate-docset "OpenCV_C++")
-  (local-set-key (kbd "C-.") 'helm-dash-at-point)
-  (local-set-key (kbd "M-.") 'jedi:goto-definition)
-  (local-set-key (kbd "M-,") 'jedi:goto-definition-pop-marker)
-
-  (wolfe/pretty-symbol-push-default)
-  (push '("def"    . ?ƒ) prettify-symbols-alist)
-  (push '("sum"    . ?Σ) prettify-symbols-alist)
-  (push '("**2"    . ?²) prettify-symbols-alist)
-  (push '("**3"    . ?³) prettify-symbols-alist)
-  (push '("None"   . ?∅) prettify-symbols-alist)
-  (push '("in"     . ?∈) prettify-symbols-alist)
-  (push '("not in" . ?∉) prettify-symbols-alist)
-  (push '("return" . ?➡) prettify-symbols-alist)
-  (prettify-symbols-mode t)
-  )
-
-;; (add-hook 'python-mode-hook 'my-python-mode-hook)
-
-(setq python-shell-interpreter "python")
-
-
 (use-package jedi-core
   :ensure t
   :commands (jedi-mode)
   :defer t
-  :hook (python-mode . my-python-mode-hook)
+  :hook (python-mode .
+		     (lambda ()
+		       (add-to-list 'company-backends 'company-jedi)
+		       
+		       (helm-dash-activate-docset "Python_3")
+		       (helm-dash-activate-docset "OpenCV_C++")
+		       
+		       ;; pretty symbols 
+		       (wolfe/pretty-symbol-push-default)
+		       (push '("def"    . ?ƒ) prettify-symbols-alist)
+		       (push '("sum"    . ?Σ) prettify-symbols-alist)
+		       (push '("**2"    . ?²) prettify-symbols-alist)
+		       (push '("**3"    . ?³) prettify-symbols-alist)
+		       (push '("None"   . ?∅) prettify-symbols-alist)
+		       (push '("in"     . ?∈) prettify-symbols-alist)
+		       (push '("not in" . ?∉) prettify-symbols-alist)
+		       (push '("return" . ?➡) prettify-symbols-alist)
+		       (prettify-symbols-mode t)))
   :config
-  (require 'python)
+  (require 'helm-dash)
+  (require 'company-jedi)
+  (setf python-environment-directory (concat init-dir "extra/python-environment"))
+  (setq python-shell-interpreter "python")
+  
   (bind-keys :map python-mode-map
-			 ("M-." . jedi:goto-definition)
-			 ("M-," . jedi:goto-definition-pop-marker)
-			 ("M-m d" . jedi:show-doc)))
+	     ("C-." . helm-dash-at-point)
+	     ("M-." . jedi:goto-definition)
+	     ("M-," . jedi:goto-definition-pop-marker)
+	     ("M-m d" . jedi:show-doc)))
 
 (use-package company-jedi
   :ensure t
@@ -79,12 +42,8 @@
   :defer t
   :after (python)
   :config
-  (setf lsp-python-ms-executable "c:/Users/hp/.emacs.d/.cache/lsp/mspyls/Microsoft.Python.LanguageServer.exe")
-  ;; :hook (python-mode . (lambda ()
-  ;; 						 (my-python-mode-hook)
-  ;; 						 (lsp))))  ; or lsp-deferred
-  )
+  (if windows-system?
+      (setf lsp-python-ms-executable "c:/Users/hp/.emacs.d/.cache/lsp/mspyls/Microsoft.Python.LanguageServer.exe")
+    (setf lsp-python-ms-executable "~/.emacs.d/.cache/lsp/mspyls/Microsoft.Python.LanguageServer")))
 
-(defmacro rm-nth (n var)
-  `(setf ,var (remove (nth ,n ,var) ,var) ))
 
