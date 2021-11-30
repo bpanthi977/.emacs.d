ed2ba2155446d777b1f09409c14c069f5c516b4c
Bibek Panthi <bpanthi977@gmail.com>
2021-11-30 07:22:57 +0545

    ace window shortcut vs M-w (copy)

--- modules/editor-config.el
+++ modules/editor-config.el
@@ -44,14 +44,14 @@
 ;; save recent files
 (use-package recentf
   :ensure t
-  :demand t 
+  :demand t
   :init
   (setq recentf-save-file (expand-file-name "recentf" savefile-dir)
-	recentf-max-saved-items 500
-	recentf-max-menu-items 15
-	;; disable recentf-cleanup on Emacs start, because it can cause
-	;; problems with remote files
-	recentf-auto-cleanup 'never)
+        recentf-max-saved-items 500
+        recentf-max-menu-items 15
+        ;; disable recentf-cleanup on Emacs start, because it can cause
+        ;; problems with remote files
+        recentf-auto-cleanup 'never)
   (recentf-mode +1))
 
 ;; highlight the current line
@@ -67,40 +67,40 @@
   :defer nil
   :config
   :bind (;; Movement [see: https://ebzzry.io/en/emacs-pairs/]
-	 ( "C-M-f" . sp-forward-sexp)
-	 ( "C-M-b" . sp-backward-sexp)
-	 ( "C-M-a" . sp-beginning-of-sexp)
-	 ( "C-M-e" . sp-end-of-sexp)
-	 
-	 ;; transpose, kill, copy
-	 ( "C-M-t" . sp-transpose-sexp)
-	 ( "C-M-k" . sp-kill-sexp)
-	 ( "C-M-w" . sp-copy-sexp)
-	 
-	 ;; wrapping, unwraping
-	 ( "M-(" . sp-wrap-round)
-	 ( "M-[" . sp-wrap-square)
-	 ( "M-\"" . sp-wrap-quote)
-	 ( "M-<backspace>" . sp-backward-unwrap-sexp)
-	 
-	 ;; slurping and barfing 
-	 ( "C-<right>" . sp-forward-slurp-sexp)
-	 ( "C-<left>" . sp-forward-barf-sexp)
+         ( "C-M-f" . sp-forward-sexp)
+         ( "C-M-b" . sp-backward-sexp)
+         ( "C-M-a" . sp-beginning-of-sexp)
+         ( "C-M-e" . sp-end-of-sexp)
 
-	 ;; ( "M-D" . sp-splice-sexp)
-	 ;; ( "C-M-<delete>" . sp-splice-sexp-killing-forward)
-	 ;; ( "C-M-<backspace>" . sp-splice-sexp-killing-backward)
-	 ;; ( "C-S-<backspace>" . sp-splice-sexp-killing-around)
+         ;; transpose, kill, copy
+         ( "C-M-t" . sp-transpose-sexp)
+         ( "C-M-k" . sp-kill-sexp)
+         ( "C-M-w" . sp-copy-sexp)
 
-	 ;; ( "C-]" . sp-select-next-thing-exchange)
-	 ;; ( "C-<left_bracket>" . sp-select-previous-thing)
-	 ;; ( "C-M-]" . sp-select-next-thing)
+         ;; wrapping, unwraping
+         ( "M-(" . sp-wrap-round)
+         ( "M-[" . sp-wrap-square)
+         ( "M-\"" . sp-wrap-quote)
+         ( "M-<backspace>" . sp-backward-unwrap-sexp)
 
-	 ;; ;;		 ( "M-F" . sp-forward-symbol)
-	 ;; ;;		 ( "M-B" . sp-backward-symbol)
+         ;; slurping and barfing
+         ( "C-<right>" . sp-forward-slurp-sexp)
+         ( "C-<left>" . sp-forward-barf-sexp)
 
-	 ;; ( "C-\"" . sp-change-inner)
-	 ( "M-i" . sp-change-enclosing))
+         ;; ( "M-D" . sp-splice-sexp)
+         ;; ( "C-M-<delete>" . sp-splice-sexp-killing-forward)
+         ;; ( "C-M-<backspace>" . sp-splice-sexp-killing-backward)
+         ;; ( "C-S-<backspace>" . sp-splice-sexp-killing-around)
+
+         ;; ( "C-]" . sp-select-next-thing-exchange)
+         ;; ( "C-<left_bracket>" . sp-select-previous-thing)
+         ;; ( "C-M-]" . sp-select-next-thing)
+
+         ;; ;;		 ( "M-F" . sp-forward-symbol)
+         ;; ;;		 ( "M-B" . sp-backward-symbol)
+
+         ;; ( "C-\"" . sp-change-inner)
+         ( "M-i" . sp-change-enclosing))
   :hook ((prog-mode . smartparens-mode))
   :init
   (require 'smartparens-config)
@@ -129,7 +129,7 @@ buffer is not visiting a file."
   :defer t
   :config
   (require 'counsel-projectile)
-  
+
   (defun bp/projectile-find-common-lisp-root (dir)
     (if (directory-files dir nil ".asd$")
         dir
@@ -139,23 +139,23 @@ buffer is not visiting a file."
           (bp/projectile-find-common-lisp-root parent)))))
 
   (push #'bp/projectile-find-common-lisp-root projectile-project-root-functions)
-  :init 
+  :init
   (setq projectile-indexing-method 'alien)
   (setq projectile-enable-caching t)
   (setq projectile-cache-file (concat init-dir "cache/projectile.cache")
-	projectile-known-projects-file (concat savefile-dir "/projectile-bookmarks.eld"))
+        projectile-known-projects-file (concat savefile-dir "/projectile-bookmarks.eld"))
 
-  (defun bp/projectile-whitespace-cleanup () 
+  (defun bp/projectile-whitespace-cleanup ()
     (interactive)
     (dolist (b (projectile-project-buffers))
       (with-current-buffer b
-	(ignore-errors (whitespace-cleanup)))))
+        (ignore-errors (whitespace-cleanup)))))
 
   :bind-keymap ("M-P" . projectile-command-map))
 
 (use-package counsel-projectile
   :ensure t
-  :defer t 
+  :defer t
   :config
   (counsel-projectile-mode 1))
 
@@ -167,6 +167,9 @@ buffer is not visiting a file."
   (winner-mode t)
   (setf aw-dispatch-always t)
   (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l))
+
+  (setq treemacs-is-never-other-window t)
+
   (smartrep-define-key bp/global-prefix-map
       "w"
     '(("o" . other-window)
@@ -176,19 +179,20 @@ buffer is not visiting a file."
       ("d" . ace-delete-window)
       ("u" . winner-undo)
       ("r" . winner-redo)))
-  
-  (bind-keys :map bp/global-prefix-map 
-	   ("w n" . make-frame)))
+
+
+  (bind-keys :map bp/global-prefix-map
+           ("w n" . make-frame)))
 
 (use-package yasnippet
   :ensure t
-  :defer t 
+  :defer t
   :preface
   (defun company-mode/backend-with-yas (backend)
     (if (and (listp backend) (member 'company-yasnippet backend))
-	backend
+        backend
       (append (if (consp backend) backend (list backend))
-	      '(:with company-yasnippet))))
+              '(:with company-yasnippet))))
 
   :after (yasnippet-snippets company-yasnippet)
   :config
@@ -199,32 +203,32 @@ buffer is not visiting a file."
   :ensure t
   :defer t
   :mode "\\.md\\'"
-  :config 
+  :config
   (setf markdown-command "/home/bpanthi/.local/bin/pandoc -f gfm"))
 
 (global-set-key (kbd "M-.") 'find-function-at-point)
 
 ;; Tramp
 (setq tramp-persistency-file-name (concat init-dir "cache/tramp"))
-;; Abbrev 
+;; Abbrev
 (setq abbrev-file-name (concat savefile-dir "/abbrev_defs"))
 
 ;; Magit
 (use-package magit
   :ensure t
   :defer t
-  :init 
+  :init
   (setf transient-history-file (concat savefile-dir "/transient/history.el")))
 
 ;; Avy
 (use-package avy
   :ensure t
-  :defer t 
+  :defer t
   :bind (:map bp/global-prefix-map
-	      ("g g" . bp/avy-goto-char-end)
-	      ("g c" . avy-goto-char)
-	      ("g s" . avy-goto-char-timer)
-	      ("g l" . avy-goto-line))
+              ("g g" . bp/avy-goto-char-end)
+              ("g c" . avy-goto-char)
+              ("g s" . avy-goto-char-timer)
+              ("g l" . avy-goto-line))
   :config
   (defun bp/avy-goto-char-end ()
     (interactive)
@@ -234,17 +238,17 @@ buffer is not visiting a file."
 
 (use-package god-mode
   :ensure t
-  :defer t 
+  :defer t
   :bind (("M-q" . god-mode-all)
-	 :map bp/global-prefix-map
-	 ("q" . god-local-mode))
+         :map bp/global-prefix-map
+         ("q" . god-local-mode))
   :config
   (define-key god-local-mode-map (kbd ".") #'repeat)
 
   (defun my-update-cursor ()
     (setq cursor-type (if (or god-local-mode buffer-read-only)
-			  'bar
-			'box)))
+                          'bar
+                        'box)))
 
   (add-hook 'god-mode-enabled-hook 'my-update-cursor )
   (add-hook 'god-mode-disabled-hook 'my-update-cursor))
@@ -265,12 +269,12 @@ buffer is not visiting a file."
   :ensure t
   :defer t
   :commands (ispell ispell-buffer)
-  :init 
+  :init
   (setq ispell-program-name "hunspell")
   (bind-keys :map bp/global-prefix-map
-	     ("e i i" . ispell)
-	     ("e i w" . ispell-word)
-	     ("e i c" . ispell-continue)))
+             ("e i i" . ispell)
+             ("e i w" . ispell-word)
+             ("e i c" . ispell-continue)))
 
 (use-package flyspell
   :ensure t
@@ -290,18 +294,18 @@ buffer is not visiting a file."
 
 (use-package expand-region
   :ensure t
-  :defer t 
+  :defer t
   :bind (("C--" . er/contract-region)
-	 ("C-=" . er/expand-region)))
+         ("C-=" . er/expand-region)))
 
 (use-package unfill
   :ensure t
   :bind (:map bp/global-prefix-map
-	      ("e u" . unfill-paragraph))
+              ("e u" . unfill-paragraph))
   :commands (unfill-paragraph unfill-toggle unfill-region))
 
 (add-hook 'text-mode-hook (lambda ()
-			    (visual-line-mode t)))
+                            (visual-line-mode t)))
 
 
 ;; (use-package origami
@@ -309,12 +313,12 @@ buffer is not visiting a file."
 ;;   :defer t
 ;;   :init
 ;;   (smartrep-define-key bp/global-prefix-map ";"
-;; 	'((";" . origami-recursively-toggle-node)
-;; 	  ("a" . origami-show-only-node)
-;; 	  ("o" . origami-open-all-nodes)
-;; 	  ("c" . origami-close-all-nodes)
-;; 	  ("u" . origami-undo)
-;; 	  ("r" . origami-redo))))
+;;      '((";" . origami-recursively-toggle-node)
+;;        ("a" . origami-show-only-node)
+;;        ("o" . origami-open-all-nodes)
+;;        ("c" . origami-close-all-nodes)
+;;        ("u" . origami-undo)
+;;        ("r" . origami-redo))))
 
 ;; (use-package discover
 ;;   :ensure t
@@ -324,7 +328,7 @@ buffer is not visiting a file."
 
 ;; Dired
 (use-package dired
-  :config 
+  :config
   (defun dired-open-file ()
     "In dired, open the file named on this line."
     (interactive)
@@ -334,13 +338,13 @@ buffer is not visiting a file."
   (define-key dired-mode-map (kbd "C-c o") 'dired-open-file))
 
 (use-package outshine
-  :ensure t 
+  :ensure t
   :defer t)
 
-;; (use-package hyperbole 
-;;   :ensure t 
+;; (use-package hyperbole
+;;   :ensure t
 ;;   :defer nil
-;;   :config 
+;;   :config
 ;;   (require 'hyperbole)
 ;;   (global-set-key (kbd "<S-down-mouse-1>") #'action-key-depress-emacs)
 ;;   (global-set-key (kbd "<S-mouse-1>") #'action-mouse-key-emacs)
