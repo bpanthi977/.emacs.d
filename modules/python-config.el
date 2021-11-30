1afce59c950e7118d8710f8d8187f771375fd9d5
Bibek Panthi <bpanthi977@gmail.com>
2021-11-30 07:26:30 +0545

    python mode and web mode use lsp

--- modules/python-config.el
+++ modules/python-config.el
@@ -1,46 +1,25 @@
-(use-package jedi-core
-  :ensure t
-  :commands (jedi-mode)
-  :defer t
-  :hook (python-mode .
-		     (lambda ()
-		       (add-to-list 'company-backends 'company-jedi)
-		       
-		       (helm-dash-activate-docset "Python_3")
-		       (helm-dash-activate-docset "OpenCV_C++")
-		       
-		       ;; pretty symbols 
-		       (wolfe/pretty-symbol-push-default)
-		       (push '("def"    . ?ƒ) prettify-symbols-alist)
-		       (push '("sum"    . ?Σ) prettify-symbols-alist)
-		       (push '("**2"    . ?²) prettify-symbols-alist)
-		       (push '("**3"    . ?³) prettify-symbols-alist)
-		       (push '("None"   . ?∅) prettify-symbols-alist)
-		       (push '("in"     . ?∈) prettify-symbols-alist)
-		       (push '("not in" . ?∉) prettify-symbols-alist)
-		       (push '("return" . ?➡) prettify-symbols-alist)
-		       (prettify-symbols-mode t)))
-  :config
-  (require 'helm-dash)
-  (require 'company-jedi)
-  (setf python-environment-directory (concat init-dir "extra/python-environment"))
-  (setq python-shell-interpreter "python")
+(defun bp/python-mode-hook ()
+  ;; helm-dash 
+  (helm-dash-activate-docset "Python_3")
+  ;(helm-dash-activate-docset "OpenCV_C++")
   
-  (bind-keys :map python-mode-map
-	     ("C-." . helm-dash-at-point)
-	     ("M-." . jedi:goto-definition)
-	     ("M-," . jedi:goto-definition-pop-marker)
-	     ("M-m d" . jedi:show-doc)))
-
-(use-package company-jedi
-  :ensure t
-  :defer t 
-  :after (company jedi-core))
+  ;; pretty symbols 
+  (wolfe/pretty-symbol-push-default)
+  (push '("def"    . ?ƒ) prettify-symbols-alist)
+  (push '("sum"    . ?Σ) prettify-symbols-alist)
+  (push '("**2"    . ?²) prettify-symbols-alist)
+  (push '("**3"    . ?³) prettify-symbols-alist)
+  (push '("None"   . ?∅) prettify-symbols-alist)
+  (push '("in"     . ?∈) prettify-symbols-alist)
+  (push '("not in" . ?∉) prettify-symbols-alist)
+  (push '("return" . ?➡) prettify-symbols-alist)
+  (prettify-symbols-mode t))
 
 (use-package lsp-python-ms
   :ensure t
   :defer t
   :after (python)
+  :hook (python-mode . #'bp/python-mode-hook)		     
   :config
   (if windows-system?
       (setf lsp-python-ms-executable "c:/Users/hp/.emacs.d/.cache/lsp/mspyls/Microsoft.Python.LanguageServer.exe")
--- modules/web-config.el
+++ modules/web-config.el
@@ -3,34 +3,11 @@
   :mode (("\\.html\\'" . web-mode)
 	 ("\\.phtml\\'" . web-mode)
 	 ("\\.tpl\\'" . web-mode)
-;;	 ("\\.php\\'" . web-mode)
 	 ("\\.[agj]sp\\'" . web-mode)
 	 ("\\.as[cp]x\\'" . web-mode)
 	 ("\\.erb\\'" . web-mode)
 	 ("\\.mustache\\'" . web-mode)
 	 ("\\.djhtml\\'" . web-mode)))
-
-(use-package php-mode
-  :ensure t
-  :defer t
-  :mode "\\.php\\'")
-
-(use-package ac-php
-  :ensure t
-  :after (helm-dash php-mode)
-  :mode "\\.php\\'"
-  :commands (ac-php-find-symbol-at-point ac-php-location-stack-back helm-dash-at-point)
-  :hook (php-mode . (lambda ()
-		      (require 'ac-php)
-		      (setq-local company-backends (cons 'company-ac-php-backend company-backends))
-		      (ac-php-core-eldoc-setup ) ;; enable eldoc
-		      (ac-php-remake-tags)
-		      (helm-dash-activate-docset "PHP")))
-  :config
-  (bind-keys :map php-mode-map
-	     ("M-." . ac-php-find-symbol-at-point)
-	     ("C-t" . ac-php-location-stack-back)
-	     ("C-." . helm-dash-at-point)))
 
 (use-package company-web
   :ensure t
