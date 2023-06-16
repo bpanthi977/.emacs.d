555d561eca5f649c5aa4bfe51e0eba562b54654c
Bibek Panthi <bpanthi977@gmail.com>
2023-06-16 19:56:25 +0545

    remove orgmode package archive. org comes with non-elpa archive now

--- modules/packages-config.el
+++ modules/packages-config.el
@@ -4,15 +4,12 @@
 (require 'package)
 ;; accessing a package repo over https on Windows is a no go, so we
 ;; fallback to http there
-(if (eq system-type 'windows-nt)
-    (progn (add-to-list 'package-archives
-			'("melpa" . "http://melpa.org/packages/") t)		   
-	   (add-to-list 'package-archives
-			'("org" . "http://orgmode.org/elpa/") t))
-  (progn (add-to-list 'package-archives
-		      '("melpa" . "https://melpa.org/packages/") t)
-	 (add-to-list 'package-archives
-		      '("org" . "https://orgmode.org/elpa/") t)))
+(cond ((eq system-type 'windows-nt)
+       (add-to-list 'package-archives
+                    '("melpa" . "http://melpa.org/packages/") t))
+      (t
+       (add-to-list 'package-archives
+                    '("melpa" . "https://melpa.org/packages/") t)))
 (package-initialize)
 
 (defun install-packages (package-list)
