aed447fa26a23aaf8063fb170d75d00dfb7e3631
Bibek Panthi <bpanthi977@gmail.com>
2025-01-23 11:07:00 -0600

    require counsel for ivy-rich

--- modules/ido-config.el
+++ modules/ido-config.el
@@ -4,17 +4,17 @@
 
 ;; (use-package ido
 ;;   :bind (("C-x C-f" . ido-find-file))
-;;   :config 
+;;   :config
 ;;   (ido-vertical-mode)
-;;   (setq ido-virtual-buffers t  
-;; 	ido-enable-prefix nil
-;; 	ido-enable-flex-matching t
-;; 	ido-create-new-buffer 'always
-;; 	ido-use-filename-at-point 'guess
-;; 	ido-max-prospects 10
-;; 	ido-save-directory-list-file (expand-file-name "ido.hist" savefile-dir)
-;; 	ido-default-file-method 'selected-window
-;; 	ido-auto-merge-work-directories-length -1))
+;;   (setq ido-virtual-buffers t
+;;      ido-enable-prefix nil
+;;      ido-enable-flex-matching t
+;;      ido-create-new-buffer 'always
+;;      ido-use-filename-at-point 'guess
+;;      ido-max-prospects 10
+;;      ido-save-directory-list-file (expand-file-name "ido.hist" savefile-dir)
+;;      ido-default-file-method 'selected-window
+;;      ido-auto-merge-work-directories-length -1))
 
 ;; (use-package ido-ubiquitous
 ;;   :after (ido)
@@ -33,7 +33,7 @@
 ;; smex, remember recently and most frequently used commands
 (use-package smex
   :ensure t
-  :config 
+  :config
   (setq smex-save-file (expand-file-name ".smex-items" savefile-dir)))
 
 
@@ -41,18 +41,18 @@
 (use-package ivy
   :ensure t
   :bind (("C-x b" . ido-switch-buffer))
-  :config 
+  :config
   (require 'smex)
   (ivy-mode t)
   (define-key ivy-minibuffer-map (kbd "S-SPC") nil)
   :init
   (setq ivy-use-virtual-buffers t
-	ivy-count-format "%d/%d "
-	ivy-re-builders-alist '( (t . ivy--regex-ignore-order))))
+        ivy-count-format "%d/%d "
+        ivy-re-builders-alist '( (t . ivy--regex-ignore-order))))
 
 (use-package ivy-rich
   :ensure t
-  :after ivy
+  :after (ivy counsel)
   :custom
   (ivy-virtual-abbreviate 'full
                           ivy-rich-switch-buffer-align-virtual-buffer t
@@ -65,19 +65,19 @@
   :ensure t
   :demand t
   :bind (("C-x C-f" . counsel-find-file)
-		 ("C-x M-f" . counsel-recentf)
-		 ("M-x" . counsel-M-x)				
-		 ("C-c u" . counsel-unicode-char)
-		 ("C-c s" . counsel-rg)
-		 ("M-s" . counsel-imenu))
+         ("C-x M-f" . counsel-recentf)
+         ("M-x" . counsel-M-x)
+         ("C-c u" . counsel-unicode-char)
+         ("C-c s" . counsel-rg)
+         ("M-s" . counsel-imenu))
   :config
   ;;(setf (alist-get 'counsel-M-x ivy-initial-inputs-alist) " ")
   ;; enable opening file as sudo
   (defadvice counsel-find-file (after find-file-sudo activate)
     "Find file as root if necessary."
     (when (and buffer-file-name
-			   (file-exists-p buffer-file-name)
-			   (not (file-writable-p buffer-file-name)))
+               (file-exists-p buffer-file-name)
+               (not (file-writable-p buffer-file-name)))
       (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
   (ivy-configure 'counsel-M-x
