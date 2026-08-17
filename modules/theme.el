4284fcbd9f9bdd422562349ed919988ee4c8e6a1
Bibek Panthi <bibek.panthi@lumalabs.ai>
2026-08-16 23:41:35 -0700

    bright diff theme in org mode

--- modules/theme.el
+++ modules/theme.el
@@ -4,6 +4,7 @@
   (load-theme 'spacemacs-dark))
 
 (require 'ansi-color)
+(require 'magit-diff)
 
 (custom-set-faces
  '(default ((t (:height 130
@@ -13,3 +14,15 @@
  '(ansi-color-bright-blue
    ((t (:foreground "#4f97d7"
         :background "#4f97d7")))))
+
+(dolist (mapping
+	 '((diff-added . magit-diff-added)
+	   (diff-indicator-added . magit-diff-added)
+	   (diff-refine-added . magit-diff-added-highlight)
+	   (diff-removed . magit-diff-removed)
+	   (diff-indicator-removed . magit-diff-removed)
+	   (diff-refine-removed . magit-diff-removed-highlight)))
+  (set-face-attribute (car mapping) nil
+		      :inherit (cdr mapping)
+		      :foreground 'unspecified
+		      :background 'unspecified))
